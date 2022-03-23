4d2d61833959de0158983f19dea72f248a2bfff9
Ivan Artamonov <i.o.artamonov@gmail.com>
2022-03-23 22:55:25 +0200

    Install HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
     filename: 'bundle.js',
   },
+  plugins: [new HtmlWebpackPlugin({
+    filename: path.resolve(__dirname, "public/index.html"),
+    template: path.resolve(__dirname, "src/index.html")
+  })],
 };
